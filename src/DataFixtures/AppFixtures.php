8b4f287b979db6a4ef32bd5f83c124cbd998755a
Khanh TRUONG <thidankhanh.jolly2022@campus-eni.fr>
2022-06-01 12:15:36 +0200

    page: créer une sortie: controller, twig, form

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,33 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Sortie;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // Utilisation de Faker//
+        $faker = Factory::create('fr_FR');
 
-        $manager->flush();
+        //Création d'une sortie
+        for ($i = 0; $i < 20; $i++) {
+            $sortie = new Sortie();
+
+            $sortie->setNom($faker->firstName())
+                ->setDateHeureDebut($faker->dateTime($max = 'now', $timezone = null))
+                ->setDuree($faker->randomDigitNotNull())
+                ->setDateLimiteInscription($faker->dateTime())
+                ->setNbinscriptionsMax($faker->randomDigitNotNull())
+                ->setInfosSortie($faker->text())
+                ->setEtat('etat');
+
+            $manager->persist($sortie);
+            $manager->flush();
+        }
+
     }
 }
