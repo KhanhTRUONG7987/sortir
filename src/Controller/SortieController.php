ffa4cbfab2e4d3e1177ee22ab021c1eb187ec551
Khanh TRUONG <thidankhanh.jolly2022@campus-eni.fr>
2022-06-02 11:03:21 +0200

    commit

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -19,47 +19,48 @@ use Symfony\Component\Form\Exception\LogicException;
 use Symfony\Component\Form\Extension\Core\DataTransformer\NumberToLocalizedStringTransformer;
 use Symfony\Component\Form\Extension\Core\DataTransformer\StringToFloatTransformer;
 
+
 class SortieController extends AbstractController
 {
-    #[Route('/sortie', name: 'sortie')]
-    public function index(SortieRepository $repo): Response
-    {
-        return $this->render('', [
-            'controller_name' => 'SortieController',
-        ]);
-    }
-
     #[Route('/sortie/createSortie', name: 'sortie_createSortie')]
-    public function createSortie(): Response
+    public function createSortie(Request $request): Response
     {
         $sortie = new Sortie();
 
-
+        //formular
         $createSortieForm = $this->createForm(CreateSortieType::class, $sortie);
+        $createSortieForm->handleRequest($request);
+
+        /*if ($createSortieForm->isSubmitted() && $createSortieForm->isValid()){
+            //faire qqchose avec les données
+            //dump($sortie)
+            EntityManager
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+
+            $this->addFlash('success','Sortie créée');
+        }*/
+        //response
         return $this->render('sortie/createSortie.html.twig', [
             "createSortieForm" => $createSortieForm->createView()
         ]);
-        /*return $this->redirectToRoute('sortie_consulterSortie', ['id'=>$sortie->getId()]);*/
+        return $this->redirectToRoute('sortie_afficherSortie', ['id'=>$sortie->getId()]);
     }
 
-    #[Route('/sortie/afficherSortie', name: 'sortie_afficherSortie')]
+    /*#[Route('/sortie', name: 'sortie')]
+    public function index(SortieRepository $sortieRepository): Response
+    {
+        return $this->render('sortie/index.html.twig', [
+            'sorties' => $sortieRepository->findAll(),
+        ]);
+    }*/
+
+    /*#[Route('/sortie/afficherSortie', name: 'sortie_afficherSortie')]
     public function show(SortieRepository $repo)
     {
-        $sorties = $repo->findAll();
-        for ($i = 0; $i < 20; $i++) {
+        $sorties = $repo->find($id, $lockMode = null, $lockVersion = null);
 
-            return $this->render('sortie/afficherUneSortie.html.twig', [
-                'sortie' => $sorties
-            ]);
-        }
+        return $this->render('sortie/afficherUneSortie.html.twig', ['id' => $sortie->getId()]);
 
-    }
-
-    #[Route('/sortie/home', name: 'home')]
-    public function home(): Response
-    {
-        return $this->render('sortie/home.html.twig');
-    }
-
-
+    }*/
 }
--- src/DataFixtures/SortieFixtures.php
+++ src/DataFixtures/SortieFixtures.php
@@ -2,6 +2,10 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Campus;
+use App\Entity\Lieu;
+use App\Entity\Sortie;
+use App\Entity\Ville;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +13,25 @@ class SortieFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $ville = $manager->getRepository(Ville::class)->findAll();
+        $lieu = $manager->getRepository(Lieu::class)->findAll();
+        $campus = $manager->getRepository(Campus::class)->findAll();
+        $sortie = new Sortie();
+        $sortie ->setNom()
+                ->setDateHeureDebut()
+                ->setDateLimiteInscription()
+                ->setNbinscriptionsMax()
+                ->setDuree()
+                ->setInfosSortie()
+                ->getSortieOrganisee($campus[0])
+                ->getSiteOrganisateur()
+                ->setDateHeureDebut()
+                ->setDateHeureDebut()
+                ->setDateHeureDebut()
+                ->setDateHeureDebut()
+                ->setDateHeureDebut();
+
+        $manager->persist($sortie);
 
         $manager->flush();
     }
