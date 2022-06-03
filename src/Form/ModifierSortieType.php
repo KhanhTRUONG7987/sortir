402beff021412356a416399f48a6206faf3e9509
Khanh TRUONG <thidankhanh.jolly2022@campus-eni.fr>
2022-06-03 14:28:52 +0200

    commit les fonctions: créer, afficher, modifier, annuler Sortie + Forms + Twigs. Gestion de Villes, Gestion de Campus

---
+++ src/Form/ModifierSortieType.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Campus;
+use App\Entity\Lieu;
+use App\Entity\Sortie;
+use App\Entity\Ville;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ModifierSortieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('dateHeureDebut')
+            ->add('dateLimiteInscription')
+            ->add('nbinscriptionsMax')
+            ->add('duree')
+            ->add('infosSortie')
+
+            ->add('campus',EntityType::class, [
+                'class' => Campus::class, 'choice_label' => 'nom', 'mapped'=>false
+            ])
+            ->add('ville',EntityType::class, [
+                'class' => Ville::class, 'choice_label' => 'nom', 'mapped'=>false
+            ])
+            ->add('lieuxSorties',EntityType::class, [
+                'class' => Lieu::class, 'choice_label' => 'rue', 'mapped'=>false
+            ])
+
+            ->add('rue',EntityType::class, [
+                'class' => Lieu::class, 'choice_label' => 'rue', 'mapped'=>false
+            ])
+
+            ->add('codePostal',EntityType::class, [
+                'class' => Ville::class, 'choice_label' => 'codePostal', 'mapped'=>false
+            ])
+
+            ->add('latitude',EntityType::class, [
+                'class' => Lieu::class, 'choice_label' => 'latitude', 'mapped'=>false
+            ])
+
+            ->add('longitude',EntityType::class, [
+                'class' => Lieu::class, 'choice_label' => 'longitude', 'mapped'=>false
+            ])
+
+            ->add('Enregistrer', SubmitType::class)
+
+            ->add('Publier', SubmitType::class)
+
+            ->add('Supprimer', SubmitType::class)
+
+            ->add('Annuler', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+        ]);
+    }
+}
