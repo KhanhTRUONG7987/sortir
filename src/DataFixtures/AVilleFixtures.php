7cc2e9294cb663e350f3d77e77b099f9f9d894fd
quentin deboudt <deboudtquentin@gmail.com>
2022-06-08 14:36:21 +0200

    regler les problemes de fixtures

--- src/DataFixtures/AVilleFixtures.php
+++ src/DataFixtures/AVilleFixtures.php
@@ -6,7 +6,7 @@ use App\Entity\Ville;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
-class VilleFixtures extends Fixture
+class AVilleFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
